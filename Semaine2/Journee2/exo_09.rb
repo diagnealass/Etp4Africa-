f6c2c6a07f61db99558f4d4f3e22219df702b213
diagnealass <diagnealass03@gmail.com>
2026-02-17 22:28:00 +0000

    Ceci est l'exo 09

---
+++ Semaine2/Journee2/exo_09.rb
@@ -0,0 +1,9 @@
+puts "C'est quoi ton prénom ?"
+print "> "
+first_name = gets.chomp
+
+puts "C'est quoi ton nom de famille ?"
+print "> "
+last_name = gets.chomp
+
+puts "Bonjour, #{first_name} #{last_name} !"
